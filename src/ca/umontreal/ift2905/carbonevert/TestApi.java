ada623c5b4e7d98e1487506d5779eaf0f1e4fc63
etigervaise <e.gervais@umontreal.ca>
2012-09-07 15:03:47 -0400

    Ajout d'un view pour pouvoir tester avec un bouton et des toast l'api seulement changer dans le AndroidManifest.xml Android:name=".TestApi"

---
+++ src/ca/umontreal/ift2905/carbonevert/TestApi.java
@@ -0,0 +1,12 @@
+package ca.umontreal.ift2905.carbonevert;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestApi extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_api);
+	}
+}
