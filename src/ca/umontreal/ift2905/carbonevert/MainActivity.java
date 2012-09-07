c2aaec841a6a066f3d9e74d1bff0dd135f614b4c
Jean-Cristoph Dury <dury.jc@gmail.com>
2012-09-07 15:55:51 -0400

    Tabs big update

--- src/ca/umontreal/ift2905/carbonevert/MainActivity.java
+++ src/ca/umontreal/ift2905/carbonevert/MainActivity.java
@@ -1,34 +1,50 @@
 package ca.umontreal.ift2905.carbonevert;
 
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
 
-	
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		TabHost tabHost=(TabHost)findViewById(R.id.tabHost);
-		tabHost.setup();
-
-		TabSpec spec1=tabHost.newTabSpec("Tab 1");
-		spec1.setContent(R.id.tab1);
-		spec1.setIndicator("Tab 1");
-
-		TabSpec spec2=tabHost.newTabSpec("Tab 2");
-		spec2.setIndicator("Tab 2");
-		spec2.setContent(R.id.tab2);
-
-		TabSpec spec3=tabHost.newTabSpec("Tab 3");
-		spec3.setIndicator("Tab 3");
-		spec3.setContent(R.id.tab3);
-
-		tabHost.addTab(spec1);
-		tabHost.addTab(spec2);
-		tabHost.addTab(spec3);
-		}
-	
+public class MainActivity extends TabActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+        TabHost tabHost = getTabHost();
+ 
+        // Tab for Activities
+        TabSpec activitiespec = tabHost.newTabSpec("Activities");
+        // setting Title and Icon for the Tab
+        activitiespec.setIndicator("Activities");
+        Intent activitiesIntent = new Intent(this, ActivitiesActivity.class);
+        activitiespec.setContent(activitiesIntent);
+ 
+        // Tab for browse
+        TabSpec browsepec = tabHost.newTabSpec("Browse");
+        browsepec.setIndicator("Browse");
+        Intent browseIntent = new Intent(this, BrowseActivity.class);
+        browsepec.setContent(browseIntent);
+ 
+        // Tab for favourites
+        TabSpec favouritespec = tabHost.newTabSpec("Favourites");
+        favouritespec.setIndicator("Favourites");
+        Intent favouritesIntent = new Intent(this, FavouritesActivity.class);
+        favouritespec.setContent(favouritesIntent);
+        
+     // Tab for favourites
+        TabSpec summaryspec = tabHost.newTabSpec("Summary");
+        summaryspec.setIndicator("Summary");
+        Intent summarysIntent = new Intent(this, SummaryActivity.class);
+        summaryspec.setContent(summarysIntent);
+ 
+        // Adding all TabSpec to TabHost
+        tabHost.addTab(activitiespec); // Adding activities tab
+        tabHost.addTab(browsepec); // Adding browse tab
+        tabHost.addTab(favouritespec); // Adding favourites tab
+        tabHost.addTab(summaryspec);//Adding summary tab
+    }
 }
---
+++ src/ca/umontreal/ift2905/carbonevert/SummaryActivity.java
@@ -0,0 +1,11 @@
+package ca.umontreal.ift2905.carbonevert;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SummaryActivity extends Activity{
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.browse_layout);
+	    }
+}
