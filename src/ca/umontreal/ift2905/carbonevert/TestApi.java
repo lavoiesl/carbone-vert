dcdd0d2a8431baf1e2f573b1f550057376ef1fb2
etigervaise <e.gervais@umontreal.ca>
2012-09-07 16:12:52 -0400

    fix merge

--- src/ca/umontreal/ift2905/carbonevert/TestApi.java
+++ src/ca/umontreal/ift2905/carbonevert/TestApi.java
@@ -2,11 +2,23 @@ package ca.umontreal.ift2905.carbonevert;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TestApi extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test_api);
+		
+		final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Toast.makeText(TestApi.this, "J'aime le bleu", Toast.LENGTH_LONG).show();
+            }
+        });
+
 	}
 }
