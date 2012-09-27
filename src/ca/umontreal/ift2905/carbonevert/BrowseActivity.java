71ab31c0c62c99443ade5cf9b4449b32c4eff3a1
Jean-Cristoph Dury <dury.jc@gmail.com>
2012-09-26 21:28:21 -0400

    added custom layout, and go back to source from transport in browse.

--- src/ca/umontreal/ift2905/carbonevert/BrowseActivity.java
+++ src/ca/umontreal/ift2905/carbonevert/BrowseActivity.java
@@ -5,27 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class BrowseActivity extends Activity {
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.browse_layout);
-
+		
 		final Button transportButton = (Button) findViewById(R.id.transportButton);
 		transportButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(final View v) {
 				// Perform action on click
-				// Intent intent = new Intent(v.getContext(), TestApi.class);
-				// startActivity(intent);
 				setContentView(R.layout.transport_layout);
-				final Button carButton = (Button) findViewById(R.id.carButton);
-				carButton.setOnClickListener(new View.OnClickListener() {
-
+				final Button sourceButton = (Button) findViewById(R.id.sourceTransportButton);
+				sourceButton.setOnClickListener(new View.OnClickListener() {
 					public void onClick(final View v) {
-						Toast.makeText(getBaseContext(), "CAR HERE",
-								Toast.LENGTH_LONG).show();
+						onCreate(savedInstanceState);
 					}
 				});
 			}
