90852ca9947ea0c240c84adbc250ca5f7d98c43f
etigervaise <e.gervais@umontreal.ca>
2012-09-28 18:32:44 -0400

    void null

--- src/ca/umontreal/ift2905/carbonevert/HistoryActivity.java
+++ src/ca/umontreal/ift2905/carbonevert/HistoryActivity.java
@@ -1,9 +1,25 @@
 package ca.umontreal.ift2905.carbonevert;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
+
+import ca.umontreal.ift2905.carbonevert.db.DatabaseHelper;
+import ca.umontreal.ift2905.carbonevert.model.AbstractData;
+import ca.umontreal.ift2905.carbonevert.model.ActivityData;
+
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.android.apptools.OrmLiteBaseListActivity;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.QueryBuilder;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -13,7 +29,7 @@ import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class HistoryActivity extends Activity {
+public class HistoryActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	private Bundle instance = null;
 
 	@Override
@@ -21,11 +37,25 @@ public class HistoryActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.history_layout);
 		instance = savedInstanceState;
+		
+		//Test initial
+		
+		final StatHistory sh = new StatHistory(getHelper());
+		sh.setFromDate(GregorianCalendar.getInstance());
+		
+		try {
+			long installed = getBaseContext().getPackageManager().getPackageInfo(getPackageName(), 0).firstInstallTime;
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+
 
 		final Button weekButton = (Button) findViewById(R.id.weekButton);
 		weekButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(final View v) {
 				// Perform action on click
+				sh.setListing(StatHistory.BY_WEEK);
+					
 				final Intent intent = new Intent(v.getContext(), TestApi.class);
 				startActivity(intent);
 			}
@@ -35,6 +65,8 @@ public class HistoryActivity extends Activity {
 		monthButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(final View v) {
 				// Perform action on click
+				sh.setListing(StatHistory.BY_MONTH);
+				
 				final Intent intent = new Intent(v.getContext(), TestApi.class);
 				startActivity(intent);
 			}
@@ -44,6 +76,8 @@ public class HistoryActivity extends Activity {
 		yearButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(final View v) {
 				// Perform action on click
+				sh.setListing(StatHistory.BY_YEAR);
+				
 				 final Intent intent = new Intent(v.getContext(), TestApi.class);
 				 startActivity(intent);
 			}
@@ -134,5 +168,60 @@ public class HistoryActivity extends Activity {
 	@Override
 	public void onBackPressed() {
 		onCreate(instance);
+	}
+	
+	private class StatHistory {
+		
+		public static final int BY_DAY = 0;
+		public static final int BY_WEEK = 1;
+		public static final int BY_MONTH = 2;
+		public static final int BY_YEAR = 3;
+		
+		private int listing;
+		
+		private List<ActivityData> activities;
+		private List<Float> total_co2 = new ArrayList<Float>();
+		
+		private final DatabaseHelper database;
+		
+		private Calendar fromDate;
+		private Calendar toDate; 
+		
+		public StatHistory(DatabaseHelper database) {
+			this.database = database;
+		}
+		
+		public void setListing (int by_list) {
+			listing = by_list;
+		}
+		
+		public void setFromDate(Calendar date) {
+			fromDate = date;	
+		}
+		
+		public void setToDate(Calendar date) {
+			toDate = date;	
+		}
+		
+		private List<ActivityData> findByDate(String date) {			
+			try {
+				final Dao<ActivityData, Integer> dao = database.getDao(ActivityData.class);
+				return dao.query(dao.queryBuilder().where().gt("date", date).prepare());
+			} catch (SQLException e) {
+				return null;
+			} catch (IndexOutOfBoundsException e) {
+				return null;
+			}
+		}
+		
+		public void fillList() {
+			activities = findByDate(fromDate.toString());
+			total_co2.set(1,(float) 0);
+			
+			for (ActivityData temp : activities) {
+				total_co2.set(1,total_co2.get(1)+temp.getCarbon());
+			}
+		}
+		
 	}
 }
