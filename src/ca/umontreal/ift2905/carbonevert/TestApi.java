6c1f962b8c8deed011ac6336aa0a56ab187fba11
Sébastien Lavoie <github@lavoie.sl>
2012-09-27 22:13:35 -0400

    reworked Web API

--- src/ca/umontreal/ift2905/carbonevert/TestApi.java
+++ src/ca/umontreal/ift2905/carbonevert/TestApi.java
@@ -1,111 +1,39 @@
 package ca.umontreal.ift2905.carbonevert;
 
-import java.sql.SQLException;
-
-import ca.umontreal.ift2905.carbonevert.db.DatabaseHelper;
-import ca.umontreal.ift2905.carbonevert.model.CategoryData;
-import ca.umontreal.ift2905.carbonevert.model.ProductData;
-
-import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
-import com.j256.ormlite.dao.Dao;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import ca.umontreal.ift2905.carbonevert.db.DatabaseHelper;
+import ca.umontreal.ift2905.carbonevert.model.ProductData;
+
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 
 public class TestApi extends OrmLiteBaseActivity<DatabaseHelper> {
-	
-	GetDataTask testData;
-	
-	private Dao<ProductData, Integer> daoP = null;
-	private ProductData product;
-	
-	private Dao<CategoryData, Integer> daoC = null;
-	private CategoryData category;
 	
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test_api);
 		
-		
-		
-		try {
-			daoP = getHelper().getDao(ProductData.class);
-			daoC = getHelper().getDao(CategoryData.class);
-		} catch (final SQLException e) {
-			Toast.makeText(getBaseContext(), "Error while listing activities", Toast.LENGTH_SHORT)
-				.show();
-			return;
-		}
+		final WebApi.GetProductTask task = new WebApi.GetProductTask(getHelper()) {
+
+			@Override
+			protected void onPostExecute(ProductData product) {
+				Toast.makeText(TestApi.this, "" + product, Toast.LENGTH_SHORT).show();
+			}
+			
+		};
 
 		final Button button = (Button) findViewById(R.id.button1);
+		
 		button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(final View v) {
-				
-				testData=new GetDataTask();
-				testData.execute("http://ask.amee.com/detail.json?category=CLM_food_lifecycle_emissions&terms=chicken&quantities=1.0+kg");
-				
+				task.loadDetails("shrimp", 1.0, "kg");
 			}
 		});
 
-	}
-	
-	private class GetDataTask extends AsyncTask<String,Integer, WebApi> {
-		@Override
-		protected void onCancelled() {
-			super.onCancelled();
-			Toast.makeText(TestApi.this, "Cancel!!!!!", Toast.LENGTH_SHORT).show();
-		}
-		
-		@Override
-		protected void onPreExecute() {
-		}
-		
-		@Override
-		protected WebApi doInBackground(String... params) {
-			
-			try {Thread.sleep(200);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			WebApi web = 
-					new WebApi(params[0]);
-			try {Thread.sleep(200);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			return web;
-		}
-		@Override
-		protected void onProgressUpdate(Integer... s) {
-
-		}
-		@Override
-		protected void onPostExecute(WebApi web) {
-			
-			if( web==null ) {
-				Toast.makeText(TestApi.this, "Probleme avec le Web", Toast.LENGTH_SHORT).show();
-				return;
-			}
-
-			try {		
-				category = web.getCategory();
-				daoC.create(category);
-				product = web.getProduct();
-				
-				product.setCategory(category);
-				daoP.create(product);
-				
-			} catch (Exception e) {
-			
-				Toast.makeText(TestApi.this, "Erreur d'insertion", Toast.LENGTH_SHORT).show();
-				throw new RuntimeException(e);
-			}
-
-		}
 	}
 }
